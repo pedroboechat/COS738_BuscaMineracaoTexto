500dae05ef2af684dbbe5ee58beaa392c2df5a62
Pedro Boechat <pedroboechat@poli.ufrj.br>
2022-05-10 09:48:55 -0300

    feat: add indexer to main

--- Trabalho_03/src/main.py
+++ Trabalho_03/src/main.py
@@ -9,6 +9,7 @@ from datetime import datetime as dt
 
 from modules.query_processor import QueryProcessor
 from modules.inverse_list import InverseListGenerator
+from modules.indexer import Indexer
 
 
 if __name__ == "__main__":
@@ -19,20 +20,25 @@ if __name__ == "__main__":
     os.makedirs("./log", exist_ok=True)
 
     # Logger configuration
-    logname = dt.utcnow().strftime("%Y-%m-%d_%Hh%Mm%Ss.%f")[:-3]
+    LOG_NAME = dt.utcnow().strftime("%Y-%m-%d_%Hh%Mm%Ss.%f")[:-3]
+    LOG_FORMAT = "%(asctime)s.%(msecs)03d [%(levelname)s] %(message)s"
+    stream_handler = logging.StreamHandler(sys.stdout)
     logging.basicConfig(
         level=logging.DEBUG,
-        format="%(asctime)s.%(msecs)03d [%(levelname)s] %(message)s",
+        format=LOG_FORMAT,
         datefmt="%Y-%m-%d %H:%M:%S",
         encoding="utf-8",
         handlers=[
-            # logging.FileHandler(f"./log/{logname}.log"),
-            logging.StreamHandler(sys.stdout)
+            logging.FileHandler(f"./log/{LOG_NAME}.log"),
+            stream_handler
         ]
     )
 
     # Instantiate and run query processor
-    # query_processor = QueryProcessor()
+    query_processor = QueryProcessor()
 
     # Instantiate and run inverse list generator
     inverse_list_generator = InverseListGenerator()
+
+    # Instantiate and run indexer
+    indexer = Indexer()
